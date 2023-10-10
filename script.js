c8b8b994f03296144b400f171d705fd6faa570b9
Rashid Agene <rashidagene4@gmail.com>
2023-10-10 12:47:58 +0000

    Second commit

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const buttonDark = document.querySelector('.dark');
-const darkImage = ./
+const image = document.getElementById('image');
+const lightImage = '/asset/switch-button.svg';
+const darkImage = '/asset/dark-switch.svg';
+const darkModeTags =document.querySelectorAll('body,header, .social, .card, .can, .dark');
+
+
 buttonDark.addEventListener('click', () => {
+    if (image.src.endsWith(darkImage)){
+        image.src = lightImage;
+    }
+    else{
+        image.src = darkImage;
+    }
+
+    darkModeTags.forEach(element =>{
+        element.classList.toggle('light-mode');
+    });
     
 });
-
-
 
 
 
