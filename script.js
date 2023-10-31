78c2668de989f4e5dbba410fdfcce4a168462056
Rashid Agene <rashidagene4@gmail.com>
2023-10-31 22:27:08 +0000

    gi

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const darkImage = '/asset/dark-switch.svg';
 const darkModeTags =document.querySelectorAll('body,header, .social, .card, .can');
 
 
-buttonDark.addEventListener('click', () => {
+/*buttonDark.addEventListener('click', () => {
     if (image.src.endsWith(darkImage)){
         image.src = lightImage;
     }
@@ -18,6 +18,79 @@ buttonDark.addEventListener('click', () => {
     });
     
 });
+fetch('data.json');*/
+// Check local storage for dark mode state and apply it if available
+const isDarkMode = localStorage.getItem('darkMode') === 'true';
+if (isDarkMode) {
+  image.src = darkImage;
+  darkModeTags.forEach(element => {
+    element.classList.add('light-mode');
+  });
+}
+
+buttonDark.addEventListener('click', () => {
+  if (image.src.endsWith(darkImage)) {
+    image.src = lightImage;
+    darkModeTags.forEach(element => {
+      element.classList.remove('light-mode');
+    });
+    // Store the state in local storage
+    localStorage.setItem('darkMode', 'false');
+  }
+   else {
+    image.src = darkImage;
+    darkModeTags.forEach(element => {
+      element.classList.add('light-mode');
+    });
+    // Store the state in local storage
+    localStorage.setItem('darkMode', 'true');
+  }
+});
+
+//Adding the json data to javascript
+/*const name1=document.querySelector('nath');
+fetch('./data.json')
+    .then(res=> res.json())
+        .then(data=> {
+            data.forEach(post=>{
+                name1.insertAdjacentElement('','');
+                });
+        });*/
+// Function to replace card content
+function replaceCardContent(cardElement, data) {
+  cardElement.querySelector(".name").textContent = data.name;
+  cardElement.querySelector(".count").textContent = data.count;
+  cardElement.querySelector(".follower").textContent = data.follower;
+  cardElement.querySelector(".new").textContent = data.new;
+  cardElement.querySelector(".likes").textContent = data.likes;
+  cardElement.querySelector(".views").textContent = data.views;
+}
+
+// Fetch and replace card data for container 1
+const can1 = document.querySelectorAll(".nath");
+fetch("data.json")
+      .then(response => response.json())
+      .then(data => {
+        const followersCountData = data.followersCount;
+        followersCountData.forEach((cardData, index) => {
+          replaceCardContent(can1[index], cardData);
+        });
+
+        const can2 = document.querySelectorAll(".up .sqs");
+        const upOverview = data.upOverview;
+        upOverview.forEach((cardData, index) => {
+          replaceCardContent(can2[index], cardData);
+        });
+        const can3 = document.querySelectorAll(".low .sqs");
+        const lowOverview = data.lowOverview;
+        lowOverview.forEach((cardData, index) => {
+          replaceCardContent(can3[index], cardData);
+        });
+      });
+
+
+
+
 
 
 
