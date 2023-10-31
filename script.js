51e571ea641204cb7f29714bc98c8e467d0761c4
Rashid Agene <rashidagene4@gmail.com>
2023-10-31 22:38:12 +0000

    git

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const buttonDark = document.querySelector('.dark');
-const image = document.getElementById('image');
-const lightImage = '/asset/switch-button.svg';
-const darkImage = '/asset/dark-switch.svg';
-const darkModeTags =document.querySelectorAll('body,header, .social, .card, .can');
-
+const buttonDark = document.querySelector(".dark");
+const image = document.getElementById("image");
+const lightImage = "/asset/switch-button.svg";
+const darkImage = "/asset/dark-switch.svg";
+const darkModeTags = document.querySelectorAll(
+  "body,header, .social, .card, .can"
+);
 
 /*buttonDark.addEventListener('click', () => {
     if (image.src.endsWith(darkImage)){
@@ -20,30 +21,29 @@ const darkModeTags =document.querySelectorAll('body,header, .social, .card, .can
 });
 fetch('data.json');*/
 // Check local storage for dark mode state and apply it if available
-const isDarkMode = localStorage.getItem('darkMode') === 'true';
+const isDarkMode = localStorage.getItem("darkMode") === "true";
 if (isDarkMode) {
   image.src = darkImage;
-  darkModeTags.forEach(element => {
-    element.classList.add('light-mode');
+  darkModeTags.forEach((element) => {
+    element.classList.add("light-mode");
   });
 }
 
-buttonDark.addEventListener('click', () => {
+buttonDark.addEventListener("click", () => {
   if (image.src.endsWith(darkImage)) {
     image.src = lightImage;
-    darkModeTags.forEach(element => {
-      element.classList.remove('light-mode');
+    darkModeTags.forEach((element) => {
+      element.classList.remove("light-mode");
     });
     // Store the state in local storage
-    localStorage.setItem('darkMode', 'false');
-  }
-   else {
+    localStorage.setItem("darkMode", "false");
+  } else {
     image.src = darkImage;
-    darkModeTags.forEach(element => {
-      element.classList.add('light-mode');
+    darkModeTags.forEach((element) => {
+      element.classList.add("light-mode");
     });
     // Store the state in local storage
-    localStorage.setItem('darkMode', 'true');
+    localStorage.setItem("darkMode", "true");
   }
 });
 
@@ -69,45 +69,21 @@ function replaceCardContent(cardElement, data) {
 // Fetch and replace card data for container 1
 const can1 = document.querySelectorAll(".nath");
 fetch("data.json")
-      .then(response => response.json())
-      .then(data => {
-        const followersCountData = data.followersCount;
-        followersCountData.forEach((cardData, index) => {
-          replaceCardContent(can1[index], cardData);
-        });
+  .then((response) => response.json())
+  .then((data) => {
+    const followersCountData = data.followersCount;
+    followersCountData.forEach((cardData, index) => {
+      replaceCardContent(can1[index], cardData);
+    });
 
-        const can2 = document.querySelectorAll(".up .sqs");
-        const upOverview = data.upOverview;
-        upOverview.forEach((cardData, index) => {
-          replaceCardContent(can2[index], cardData);
-        });
-        const can3 = document.querySelectorAll(".low .sqs");
-        const lowOverview = data.lowOverview;
-        lowOverview.forEach((cardData, index) => {
-          replaceCardContent(can3[index], cardData);
-        });
-      });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    const can2 = document.querySelectorAll(".up .sqs");
+    const upOverview = data.upOverview;
+    upOverview.forEach((cardData, index) => {
+      replaceCardContent(can2[index], cardData);
+    });
+    const can3 = document.querySelectorAll(".low .sqs");
+    const lowOverview = data.lowOverview;
+    lowOverview.forEach((cardData, index) => {
+      replaceCardContent(can3[index], cardData);
+    });
+  });
